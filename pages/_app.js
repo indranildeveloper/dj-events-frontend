600c56f8421b74d91b70b33009ab10749393caea
Indranil Halder <indranilhalder.dev@gmail.com>
2023-01-14 18:07:22 +0530

    Small Fix

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,12 @@ const poppins = Poppins({
   subsets: ["latin"],
 });
 
-export default function App({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
   return (
     <main className={poppins.className}>
       <Component {...pageProps} />
     </main>
   );
-}
+};
+
+export default App;
